69cc7b776f1020bb642ffd4ab170bc40b6a9351a
Timo Lösch <github@tlxhost.de>
2021-01-29 21:13:04 +0100

    default pid values for hotend 220 and bed 70

--- config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 2.0/Configuration.h
+++ config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 2.0/Configuration.h
@@ -495,9 +495,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  24.93
+    #define DEFAULT_Ki   2.22
+    #define DEFAULT_Kd  69.95
   #endif
 #endif // PIDTEMP
 
@@ -534,9 +534,9 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  #define DEFAULT_bedKp 50.71
-  #define DEFAULT_bedKi 9.88
-  #define DEFAULT_bedKd 173.43
+  #define DEFAULT_bedKp 125.72
+  #define DEFAULT_bedKi 24.50
+  #define DEFAULT_bedKd 430.10
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
